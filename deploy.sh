b94336324545e86a84f02a05f3686e148f3d32a2
venkatarajv <venkatrajv143@gmailcom>
2020-09-23 22:08:42 +0530

    change workflow dynamically

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+POD=`kubectl get pods --namespace $1 -l "component=web,app=airflow" -o jsonpath="{.items[0].metadata.name}"`
+echo "Pod name is $POD "
 for entry in "dags"/*
 do
-  echo "`kubectl cp $entry airflow-1600869590-web-7cdbbffc98-t8gfq:/opt/airflow/dags -n=colpal-dev`"
+  echo "`kubectl cp $entry $POD:/opt/airflow/dags -n=colpal-dev`"
 done
 

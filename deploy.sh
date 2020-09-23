a7c294a59a2a834afe67ed4aa68c5e1f5fd0c517
venkatarajv <venkatrajv143@gmailcom>
2020-09-23 23:25:35 +0530

    changes in bash script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-NAMESPACE=$1
+ARG=$1
+
+if [ "$ARG" == "colpal-dev" ]
+then 
+	NAMESPACE=$ARG
+elif [ "$ARG" == "colpal-test" ]
+then
+        NAMESPACE=$ARG
+elif [ "$ARG" == "colpal-mockprod" ]
+then
+        NAMESPACE=$ARG
+elif [ "$ARG" ==  "colpal-prod" ]
+then
+        NAMESPACE=$ARG
+else 
+	echo "NO NAMESPACES FOUND"
+	exit 0
+fi
+
 POD=`kubectl get pods --namespace $NAMESPACE -l "component=web,app=airflow" -o jsonpath="{.items[0].metadata.name}"`
 
-echo "Pod name is $POD "
-echo "Pod name is $NAMESPACE "
 for entry in "dags"/*
 do
-  echo "`kubectl cp $entry $POD:/opt/airflow/dags -n=$NAMESPACE`"
+  kubectl cp $entry $POD:/opt/airflow/dags -n=$NAMESPACE
 done
 

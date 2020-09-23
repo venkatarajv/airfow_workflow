6ba8fa28e6fdb6a8633413c076118699890209af
venkatarajv <venkatrajv143@gmailcom>
2020-09-23 22:54:05 +0530

    updated workflow

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-POD=`kubectl get pods --namespace $1 -l "component=web,app=airflow" -o jsonpath="{.items[0].metadata.name}"`
+NAMESPACE=$1
+POD=`kubectl get pods --namespace $NAMESPACE -l "component=web,app=airflow" -o jsonpath="{.items[0].metadata.name}"`
+
 echo "Pod name is $POD "
+echo "Pod name is $NAMESPACE "
 for entry in "dags"/*
 do
-  echo "`kubectl cp $entry $POD:/opt/airflow/dags -n=colpal-dev`"
+  echo "`kubectl cp $entry $POD:/opt/airflow/dags -n=$NAMESPACE`"
 done
 
